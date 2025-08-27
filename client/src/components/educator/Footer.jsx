169a85ce7164da5d2a2ae9090f3848e3829af101
Heshan Asmadala <Heshanasmadala@gmail.com>
2025-08-27 13:11:18 +0530

    second commit

---
+++ client/src/components/educator/Footer.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { assets } from '../../assets/assets'
+
+const Footer = () => {
+  return (
+    <footer className='flex md:flex-row flex-col-reverse
+    items-center justify-between text-left px-8 border-t'>
+      <div className='flex items-center gap-4'>
+        <img src={assets.logo} alt="logo" className='hidden md:block w-20'/>
+        <div className='hidden md:block h-7 w-pc bg-gray-500/60'></div>
+        <p className='py-4 text-center text-xs md:text-sm text-gray-500'>
+          Copyright 2025 Shanzz. All rights reserved.
+        </p>
+      </div>
+      <div className='flex items-center gap-3 max-md:mt-4'>
+        <a href="#">
+          <img src={assets.facebook_icon} alt="facebook_icon" />
+        </a>
+        <a href="#">
+          <img src={assets.twitter_icon} alt="twitter_icon" />
+        </a>
+        <a href="#">
+          <img src={assets.instagram_icon} alt="instagram_icon" />
+        </a>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
